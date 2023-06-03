96290ace578ba2a3f5265e26fdaafe996eadaa11
Lisichka <zhu.ivanova@mail.ru>
2023-06-03 20:02:39 +0700

    no revers commit del product

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.on(message, async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-  const {queryId, products = []} = req.body;
+  const {queryId} = req.body;
   try {
       await bot.answerWebAppQuery(queryId, {
           type: 'article',
