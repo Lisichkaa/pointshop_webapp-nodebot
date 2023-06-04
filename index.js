1f3b1add836286389d0a26a82b33a36b4aa26699
Lisichka <zhu.ivanova@mail.ru>
2023-06-04 13:36:51 +0700

    del trycatch and add products

--- index.js
+++ index.js
@@ -25,22 +25,19 @@ bot.on('message', async (msg) => {
         })
     }
   });
+  
 app.post('/web-data', async (req, res) => {
-    const {queryId} = req.body;
-    // try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю покупкой, вы приобрели товар на сумму`
-            }
-        })
-        return res.status(200).json({});
-    // } catch (e) {
-    //     return res.status(500).json({})
-    // }
-})
+    const {queryId, products} = req.body;
+    await bot.answerWebAppQuery(queryId, {
+        type: 'article',
+        id: queryId,
+        title: 'Успешная покупка',
+        input_message_content: {
+            message_text: ` Поздравляю покупкой, вы приобрели товар на сумму`
+        }
+    })
+    return res.status(200).json({});
+    })
 
 const PORT = 8000;
 
